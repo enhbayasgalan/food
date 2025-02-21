083a08de1f175376326415078df0132bdd5a9feb
enhbayasgalan <enhuush0109@gmail.com>
2025-02-21 15:23:22 +0800

    login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
 "use client"
-import { SignupPassword } from "@/components/SignuoPassword";
-import { SignUp } from "@/components/signup";
 import { useState } from "react";
 
 
 export default function Home() {
-  const[step, setStep] = useState<number>(1)
   return (
-    <div className=" w-screen h-screen">
-     
-      { step === 1 ? <SignUp setStep={setStep}/>: <> </>}  
-      { step === 2 ? <SignupPassword/>: <> </>}  
-
-      
-    </div>
+    <div></div>
   );
 }
